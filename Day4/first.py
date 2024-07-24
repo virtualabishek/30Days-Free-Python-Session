c5f3bc032f6cdc07fb0432e1d581129beea1b32d
virtualabishek <717abishekneupane@gmail.com>
2024-07-24 17:29:32 +0545

    Dat 0 4

---
+++ Day4/first.py
@@ -0,0 +1,11 @@
+# Airthmetical Operation
+# addition of 2 number
+
+a = 10.32323
+b = 20
+
+sum = a + b
+
+print(sum)
+
+
---
+++ Day4/third.py
@@ -0,0 +1,8 @@
+''' Suppose you have to divide 14 pens among 3 students equally.
+    You need to find out how many pens each student would get (if pens must be divided equally), and the number of remaining pens that cannot be divided.
+    To solve this problem, create a variable named pen_number and assign 14 to it.
+    Create another variable named student_number and assign 3 to it.
+    Compute the number of pens each student will get and print it.
+    Compute the number of remaining pens and print it.    
+    '''
+
