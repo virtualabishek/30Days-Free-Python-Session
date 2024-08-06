e11a77cc83ea0f1c88d5ffda655eecb32550bc4c
Abishek Neupane <88627841+virtualabishek@users.noreply.github.com>
2024-08-06 12:02:00 +0000

    homework given

--- day15/6.py
+++ day15/6.py
@@ -1,1 +1,7 @@
 #Write a program to find the multiplication table of an integer (entered by the user) from 6 to 9.
+
+num = int(input("Enter the number: "))
+product = 1;
+for i in range(6,10):
+    product = num * i
+    print(f"{num} * {i} = {product}")
