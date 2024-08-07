f8bdbbcb6f1665892158f0d7fbbfb3d8f43d61c6
Abishek Neupane <88627841+virtualabishek@users.noreply.github.com>
2024-08-07 12:16:02 +0000

    done for today

---
+++ day16/4.py
@@ -0,0 +1,10 @@
+## Perform some calculation on argument of the function
+
+def addNum(a,b,c):
+    result = a + b + c
+    print(f"The sum is: {result}")
+
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+num3 = int(input("Enter the number: "))
+addNum(num1, num2, num3)
---
+++ day16/8.py
@@ -0,0 +1,11 @@
+
+def addNum(a,b):
+    sum = a + b
+    return sum
+
+
+ans = (addNum(3,3))
+
+
+if ans %2 == 0:
+    print("Even Number")
