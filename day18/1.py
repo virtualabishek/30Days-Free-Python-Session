0dfb70dc085173db18efd14eb8eb2781ec65b728
Abishek Neupane <88627841+virtualabishek@users.noreply.github.com>
2024-08-13 11:37:31 +0000

    going

---
+++ day18/1.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    add = a + b
+    print(add)
+
+sum(1,2)
+# addNumber = sum(1,2)
+# if(addNumber>9):
+#     print("the sum is greater than 9.")
---
+++ day18/2.py
@@ -0,0 +1,1 @@
+# Write a program to create a list containing 5 elements and find its length using len().
